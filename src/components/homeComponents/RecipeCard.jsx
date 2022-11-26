6c7e466148abbba7a1b0350705db9b45f303a3b5
Peyton Moss <pmoss94@gmail.com>
2022-11-26 15:22:31 -0700

    recipe card outlining

--- src/components/homeComponents/RecipeCard.jsx
+++ src/components/homeComponents/RecipeCard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function RecipeCard () {
     return (
-        <div>
-            <div>
+        <div className="recipe-card">
+            <div className="img-div">
                 <img src="https://www.simplyrecipes.com/thmb/F2Yo7A74ZG6X9Yw6kycKaARDQL8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2018__02__Creme-Brulee-LEAD-VERTICAL-c99fa6c67f20441c9430df056b6e9d6a.jpg" alt="recipe photo" />
             </div>
             <div>
