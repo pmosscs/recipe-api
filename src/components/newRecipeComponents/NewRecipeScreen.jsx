88109839c967069dbdf4c9c50a912ffcb8eb6000
Peyton Moss <pmoss94@gmail.com>
2022-12-01 09:35:25 -0700

    adding form components

--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -4,7 +4,7 @@ const NewRecipeScreen = () => {
   return (
     <section>
       <h1>Tell us about your Recipe!</h1>
-      {/* Here you will have a large form. Be prepared, part 4 will have you build this form in detail, and part 5 will have you style it. Do your best! */}
+       
     </section>
   );
 };
